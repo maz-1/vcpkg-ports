23c7d07ce82e93b842cd3321b9faafbe1eb9b301
maz-1 <ohmygod19993@gmail.com>
2022-03-01 00:38:22 +0800

    update

--- libcitygml/portfile.cmake
+++ libcitygml/portfile.cmake
@@ -9,13 +9,35 @@ vcpkg_from_github(
         0002_remove_glu_dep.patch
 )
 
-#LIBCITYGML_OSG_PLUGIN_INSTALL_DIR
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    list(APPEND ADDITIONAL_OPTIONS
+        -DLIBCITYGML_OSGPLUGIN=ON
+        -DLIBCITYGML_DYNAMIC=ON
+    )
+else()
+    list(APPEND ADDITIONAL_OPTIONS
+        -DLIBCITYGML_OSGPLUGIN=OFF
+        -DLIBCITYGML_DYNAMIC=OFF
+    )
+endif()
+
+if(LIBCITYGML_STATIC_CRT STREQUAL "static")
+    list(APPEND ADDITIONAL_OPTIONS
+        -DLIBCITYGML_STATIC_CRT=ON
+    )
+else()
+    list(APPEND ADDITIONAL_OPTIONS
+        -DLIBCITYGML_STATIC_CRT=OFF
+    )
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DLIBCITYGML_OSGPLUGIN=ON
         -DLIBCITYGML_USE_GDAL=ON
+        ${ADDITIONAL_OPTIONS}
 )
 
 file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/share/libcitygml)
@@ -25,23 +47,31 @@ vcpkg_fixup_cmake_targets()
 vcpkg_copy_pdbs()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-#file(INSTALL ${SOURCE_PATH}/src/refimpl/E57RefImplConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/e57refimpl/)
 
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools)
-file(RENAME ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/tools/osg)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
-file(RENAME ${CURRENT_PACKAGES_DIR}/tools/osg/pkgconfig ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
-file(RENAME ${CURRENT_PACKAGES_DIR}/tools/osg/citygml.lib ${CURRENT_PACKAGES_DIR}/lib/citygml.lib)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/libcitygml)
-file(RENAME ${CURRENT_PACKAGES_DIR}/bin/citygmlOsgViewer.exe ${CURRENT_PACKAGES_DIR}/tools/libcitygml/citygmlOsgViewer.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/bin/citygmltest.exe ${CURRENT_PACKAGES_DIR}/tools/libcitygml/citygmltest.exe)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/tools/osg)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/tools/osg/pkgconfig ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/tools/osg/citygml.lib ${CURRENT_PACKAGES_DIR}/lib/citygml.lib)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/libcitygml)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/bin/citygmlOsgViewer.exe ${CURRENT_PACKAGES_DIR}/tools/libcitygml/citygmlOsgViewer.exe)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/bin/citygmltest.exe ${CURRENT_PACKAGES_DIR}/tools/libcitygml/citygmltest.exe)
+else()
+    
+endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/tools)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib ${CURRENT_PACKAGES_DIR}/debug/tools/osg)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/tools/osg/pkgconfig ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/tools/osg/citygmld.lib ${CURRENT_PACKAGES_DIR}/debug/lib/citygmld.lib)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/tools/libcitygml)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/citygmlOsgViewer.exe ${CURRENT_PACKAGES_DIR}/debug/tools/libcitygml/citygmlOsgViewer.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/citygmltest.exe ${CURRENT_PACKAGES_DIR}/debug/tools/libcitygml/citygmltest.exe)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/tools)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib ${CURRENT_PACKAGES_DIR}/debug/tools/osg)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/tools/osg/pkgconfig ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/tools/osg/citygmld.lib ${CURRENT_PACKAGES_DIR}/debug/lib/citygmld.lib)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/tools/libcitygml)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/citygmlOsgViewer.exe ${CURRENT_PACKAGES_DIR}/debug/tools/libcitygml/citygmlOsgViewer.exe)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/citygmltest.exe ${CURRENT_PACKAGES_DIR}/debug/tools/libcitygml/citygmltest.exe)
+else()
+    
+endif()
